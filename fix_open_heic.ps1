5c6bbfd05ac8a4f0fb72a5e1a58f98fbf48d1b95
哈哈是我啦 <25686646+ab449449@users.noreply.github.com>
2022-06-29 01:40:10 +0800

    Update fix_open_heic.ps1

--- fix_open_heic.ps1
+++ fix_open_heic.ps1
@@ -1,3 +1,7 @@
-echo "Done,closing window"
+Invoke-WebRequest https://raw.githubusercontent.com/ab449449/fix_open_heic/master/HEIF.Appx -OutFile ./HEIF.Appx
+Invoke-WebRequest https://raw.githubusercontent.com/ab449449/fix_open_heic/master/HEVC.Appx -OutFile ./HEVC.Appx
+Add-AppxPackage HEVC.Appx
+Add-AppxPackage HEIF.Appx
+echo "Done,window will be closed in 10 seconds"
 Start-Sleep 10
 exit
