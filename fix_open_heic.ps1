0dc1ca2a54859e446d81a3a8f750943a53c6481f
哈哈是我啦 <25686646+ab449449@users.noreply.github.com>
2022-06-29 01:09:10 +0800

    Update fix_open_heic.ps1

--- fix_open_heic.ps1
+++ fix_open_heic.ps1
@@ -1,5 +1,1 @@
-Invoke-WebRequest https://raw.githubusercontent.com/ab449449/fix_open_heic/master/HEIF.Appx -OutFile ./HEIF.Appx
-Invoke-WebRequest https://raw.githubusercontent.com/ab449449/fix_open_heic/master/HEVC.Appx -OutFile ./HEVC.Appx
-Add-AppxPackage HEVC.Appx
-Add-AppxPackage HEIF.Appx
-Write-Host "執行完畢，請關閉本視窗"
+Write-Host "hello"
