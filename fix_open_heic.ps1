b7c994415270bf179ea1206240a40e142c51983a
哈哈是我啦 <25686646+ab449449@users.noreply.github.com>
2022-06-30 00:45:01 +0800

    Update fix_open_heic.ps1

--- fix_open_heic.ps1
+++ fix_open_heic.ps1
@@ -1,6 +1,5 @@
 Start-BitsTransfer -Source https://raw.githubusercontent.com/ab449449/fix_open_heic/master/HEVC.Appx -Destination ./HEVC.Appx
 Start-BitsTransfer -Source https://raw.githubusercontent.com/ab449449/fix_open_heic/master/HEIF.Appx -Destination ./HEIF.Appx
-Get-BitsTransfer | Complete-BitsTransfer
 Add-AppxPackage HEVC.Appx
 Add-AppxPackage HEIF.Appx
 echo "Done,this window will be closed in 10 seconds"
